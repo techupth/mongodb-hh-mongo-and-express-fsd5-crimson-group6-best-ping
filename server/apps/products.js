b4ca0bbce0e4c096d8e663222e3c26668ba8d2a2
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2024-01-16 04:08:02 +0700

    submit assignment

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,80 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const allProducts = await collection.find({}).limit(10).toArray();
+    return res.json({ data: allProducts });
+  } catch (error) {
+    return res.json({
+      message: `${error}`,
+    });
+  }
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.id);
+    const productById = await collection.findOne({ _id: productId });
+    return res.json({ data: productById });
+  } catch (error) {
+    return res.json({
+      message: `${error}`,
+    });
+  }
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productsData = { ...req.body, created_at: new Date() };
+    const newProducts = await collection.insertOne(productsData);
 
-productRouter.put("/:id", (req, res) => {});
+    return res.json({
+      message: `Create product (${newProducts.insertedId}) successfully`,
+    });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const newProductData = { ...req.body, modified_at: new Date() };
+    const productId = new ObjectId(req.params.id);
+    await collection.updateOne(
+      {
+        _id: productId,
+      },
+      { $set: newProductData }
+    );
+    return res.json({
+      message: `Movie record ${productId} updated successfully`,
+    });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.id);
+    await collection.deleteOne({ _id: productId });
+    return res.json({
+      message: `Delete moive record ${productId} successfully`,
+    });
+  } catch (error) {
+    return res.json({
+      message: `${error}`,
+    });
+  }
+});
 
 export default productRouter;
